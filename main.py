47e0f830a937513fb463b4a673a500b16e15ed2c
visdomr <runingback33@gmail.com>
2025-03-17 16:05:40 -0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+wordlist = ("processor", "graphics", "mouse", "monitor", "cable", "keyboard", "server", "networking", "firewall", "code", "programming", "storage")
+
+underscores = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"]
+
+word = random.choice(wordlist)
+
