1dd486264ff686089eccbd5f66fde0649c277599
visdomr <runingback33@gmail.com>
2025-03-18 16:23:28 -0700

    fixed inital start of game logic.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import random
 
-wordlist = ("processor", "graphics", "mouse", "monitor", "cable", "keyboard", "server", "networking", "firewall", "code", "programming", "storage")
-
-underscores = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"]
+wordlist = ("processor", "graphics", "mouse", "monitor", "cable", "keyboard",
+            "server", "networking", "firewall", "code", "programming",
+            "storage", "fiber", "client", "compile", "computer", "laptop")
 
 word = random.choice(wordlist)
+underscores = ["_"] * len(word)
+print(" ".join(underscores))
+points = 0
 
+while True:
+    letter = input("Enter a letter: ")
+    if letter in word:
+        print("Correct!")
+        for i in range(len(word)):
+            if word[i] == letter:
+                underscores[i] = letter
+        print(" ".join(underscores))
+    else:
+        print("Incorrect.")
