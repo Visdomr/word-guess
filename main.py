3c30898984996657c9950a6357acab8e0264bbec
Visdomr <runingback33@gmail.com>
2025-03-19 23:20:52 -0700

    cleaned up a little and added try again logic, win condition and play again broken currently.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ wordlist = ("processor", "graphics", "mouse", "monitor", "cable", "keyboard",
 word = random.choice(wordlist)
 underscores = ["_"] * len(word)
 print(" ".join(underscores))
-points = 0
 wrong = 0
 
 while True:
@@ -26,5 +25,17 @@ while True:
         print("Incorrect.")
         wrong += 1
         if wrong == 6:
-            print("You've missed 6 times, you lose.")
+            print("You've missed 6 times, you lose. The word was: ", word)
+            play_again = input(
+                "Would you like to continue playing? yes/no, y/n: ").lower()
+            if play_again == "y" or play_again == "yes":
+                word = random.choice(wordlist)
+                wrong = 0
+
+            elif play_again == "n" or play_again == "no":
+                print("Thanks for playing! Goodbye.")
+                break
+            else:
+                ("Please enter yes or no, y or n only.")
+
             break
